8a4eabb1d83844104172756652dd9baced92ba86
mbutler <matthewtbutler@gmail.com>
2018-07-03 23:30:33 -0500

    webpack, babel, mocha, chai

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+
+// SAMPLE DATA FOR CONFIGURATION TESTING
+export const DIRECTION_COORDINATES = [
+    [1, 0, -1],
+    [1, -1, 0],
+    [0, -1, 1],
+    [-1, 0, 1],
+    [-1, 1, 0],
+    [0, 1, -1]
+]
